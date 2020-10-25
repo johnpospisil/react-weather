bd2346395a4e71683bee391936b2b540bb4d6e8e
johnpospisil <audiofreak7@gmail.com>
2020-10-25 12:58:53 -0700

    Add a location-box and weather-box to the App.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
 import React from "react";
 
 const api = {
-  key: process.env.OPEN_WEATHER_API_KEY,
+  key: process.env.REACT_APP_OPEN_WEATHER_API_KEY,
   base: "api.openweathermap.org/data/2.5/",
 };
 
 function App() {
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    console.log(day);
+    return `${day} ${month} ${date}, ${year}`;
+  };
+
   return (
     <div className="App">
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Search..." />
+          <div className="location-box">
+            <div className="location">New York, NY</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">20° C</div>
+            <div className="weather">Sunny</div>
+          </div>
         </div>
       </main>
     </div>
